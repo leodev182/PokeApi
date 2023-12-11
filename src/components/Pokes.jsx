c55915f7dcf3ef99d4877a0fde065ff14a78b9c4
Leonardo Duque <leoartdev@gmail.com>
2023-12-11 00:52:21 -0300

    Limpieza general de la APP

--- src/components/Pokes.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Pokes = () => {
-  return <div>Pokes</div>;
-};
--- src/views/Pokemon.jsx
+++ src/views/Pokemon.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 
 import { useParams } from "react-router-dom";
@@ -37,7 +36,6 @@ const Pokemon = () => {
     return <p>No se encontró el Pokémon</p>;
   }
 
-  //   console.log(selectedPokemon);
   return (
     <div
       style={{
